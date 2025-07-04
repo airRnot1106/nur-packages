3975c97c69cde310c48602d40699feef03db2228
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-04 22:30:27 +0900

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,51 @@
 {
-  description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+  description = "airRnot's NUR repository";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          legacyPackages = import ./default.nix { inherit pkgs; };
+          packages = pkgs.lib.filterAttrs (_: v: pkgs.lib.isDerivation v) config.legacyPackages;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              nil
+            ];
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          };
+
+          pre-commit = import ./pre-commit.nix { inherit config; };
+          treefmt = import ./treefmt.nix;
+        };
     };
 }
---
+++ pre-commit.nix
@@ -0,0 +1,11 @@
+{ config }:
+{
+  check.enable = true;
+  settings.hooks = {
+    nil.enable = true;
+    treefmt = {
+      enable = true;
+      package = config.treefmt.build.wrapper;
+    };
+  };
+}
