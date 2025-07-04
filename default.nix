7cab1579768ebda4e70a25a5961f99019fec1570
airRnot1106 <yngw.rintaro@gmail.com>
2025-07-04 22:43:05 +0900

    feat: add similarity

--- default.nix
+++ default.nix
@@ -16,6 +16,8 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
+  git-ombl = pkgs.callPackage ./pkgs/git-ombl { };
+  similarity = pkgs.callPackage ./pkgs/similarity { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
---
+++ pkgs/similarity/default.nix
@@ -0,0 +1,24 @@
+{
+  rustPlatform,
+  fetchFromGitHub,
+  versionCheckHook,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "similarity";
+  version = "0.2.4";
+
+  src = fetchFromGitHub {
+    owner = "mizchi";
+    repo = "similarity";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Z2ZaKBpq7N8KIX8nOzPhm8evfoUxBzaAK0+4cU9qBDE=";
+  };
+
+  cargoHash = "sha256-oYqdCHGY6OZSbYXhjIt20ZL2JkZP7UEOhn0fhuZQnZo=";
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgram = "${placeholder "out"}/bin/${finalAttrs.pname}-ts";
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
+})
