a97e65a27c4f0f966a98d4995199c449397e889d
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-05 00:23:56 +0900

    feat: add version-lsp unstable-2026-01-03

--- default.nix
+++ default.nix
@@ -17,6 +17,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  version-lsp = pkgs.callPackage ./pkgs/version-lsp { };
   volt = pkgs.callPackage ./pkgs/volt { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
