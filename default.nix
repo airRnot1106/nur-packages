353f4ff98b7ddedffa658e8a7940759c10c9e949
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-05 10:04:43 +0900

    feat: add git-ombl 0.1.1

--- default.nix
+++ default.nix
@@ -17,6 +17,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  git-ombl = pkgs.callPackage ./pkgs/git-ombl { };
   version-lsp = pkgs.callPackage ./pkgs/version-lsp { };
   volt = pkgs.callPackage ./pkgs/volt { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
---
+++ pkgs/git-ombl/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  libiconv,
+  openssl,
+  pkg-config,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "git-ombl";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "airRnot1106";
+    repo = "git-ombl";
+    rev = "v${version}";
+    hash = "sha256-pqCmb31Rh37cWpKK4fvpyfjWkc2qPmKXkBM1c2CCkr8=";
+  };
+
+  cargoHash = "sha256-u0pF7FB/MkKp+iiCrcScFbePpytu4SMD+txTOxN/CQE=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+  buildInputs = [
+    libiconv
+    openssl
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Git blame wrapper to display the full commit history for a line";
+    homepage = "https://github.com/airRnot1106/git-ombl";
+    license = lib.licenses.mit;
+    mainProgram = "git-ombl";
+  };
+}
