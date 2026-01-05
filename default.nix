bb56144ea8c109e553398dc10562d74779670c41
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-05 10:17:35 +0900

    feat: add pim 0.1.1

--- default.nix
+++ default.nix
@@ -18,6 +18,7 @@
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
   git-ombl = pkgs.callPackage ./pkgs/git-ombl { };
+  pim = pkgs.callPackage ./pkgs/pim { };
   version-lsp = pkgs.callPackage ./pkgs/version-lsp { };
   volt = pkgs.callPackage ./pkgs/volt { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
---
+++ pkgs/pim/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "pim";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "airRnot1106";
+    repo = "pim";
+    rev = "v${version}";
+    hash = "sha256-cudN5lB0jLgG21cYAearbWylJ1N0vlX82Z6N6LRTt7U=";
+  };
+
+  cargoHash = "sha256-CfNRB8AfitOP6RwR5bZQq3a64jRWeYDZMV3Yjwlfcy8=";
+
+  meta = {
+    description = "Pim is a lightweight converter for CSS units";
+    homepage = "https://github.com/airRnot1106/pim";
+    license = lib.licenses.mit;
+    mainProgram = "pim";
+  };
+}
