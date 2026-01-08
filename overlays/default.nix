9293835e0fc16e63a6a6001511f26fe5188b9e1d
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-08 21:19:21 +0900

    fix: change final to prev

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,9 @@
 {
   # Default overlay that adds all packages to nixpkgs
   default = final: prev: {
-    git-ombl = final.callPackage ../pkgs/git-ombl { };
-    pim = final.callPackage ../pkgs/pim { };
-    version-lsp = final.callPackage ../pkgs/version-lsp { };
-    volt = final.callPackage ../pkgs/volt { };
+    git-ombl = prev.callPackage ../pkgs/git-ombl { };
+    pim = prev.callPackage ../pkgs/pim { };
+    version-lsp = prev.callPackage ../pkgs/version-lsp { };
+    volt = prev.callPackage ../pkgs/volt { };
   };
 }
