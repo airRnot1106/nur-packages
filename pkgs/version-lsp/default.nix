30d087a8b2d41493d25aa419b7386a3526859271
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-01-16 22:44:41 +0900

    feat: update version-lsp unstable-2026-01-03 ->  unstable-2026-01-16

--- pkgs/version-lsp/default.nix
+++ pkgs/version-lsp/default.nix
@@ -6,16 +6,18 @@
 
 rustPlatform.buildRustPackage {
   pname = "version-lsp";
-  version = "unstable-2026-01-03";
+  version = "unstable-2026-01-16";
 
   src = fetchFromGitHub {
     owner = "skanehira";
     repo = "version-lsp";
-    rev = "f6a0d96d4561aa73c668bbd8656d2efe133f94f3";
-    hash = "sha256-0RYzgOpvlrtA84LHGj+wxR198ghCz5RP/PSawGHgk/Y=";
+    rev = "ede6ac749e06731b5fbed84d3382766e6a1ae321";
+    hash = "sha256-E+Nyr9fDOOElPNSYSYj0XTaVWbF4ZuNTCouz/qgE4zg=";
   };
 
-  cargoHash = "sha256-obTLTrElqa71DosFsdh174wrDRH5uBBGW58n6ejk9qo=";
+  cargoHash = "sha256-Tt0LaYDr8hq+DENfLA1ayM80sNNu0QA/il4gsnreNlY=";
+
+  doCheck = false;
 
   meta = {
     description = "A Language Server Protocol (LSP) implementation that provides version checking diagnostics for package dependency files";
