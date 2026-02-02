f53ca4805cb4708a455b500a1b0567d44b82c806
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-02-02 21:33:49 +0900

    feat: update version-lsp 0.4.0 -> 0.4.0-unstable-2026-02-01

--- pkgs/version-lsp/default.nix
+++ pkgs/version-lsp/default.nix
@@ -2,43 +2,30 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
-  pkg-config,
   openssl,
-  cacert,
-  makeBinaryWrapper,
+  pkg-config,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "version-lsp";
-  version = "0.4.0";
+  version = "0.4.0-unstable-2026-02-01";
 
   src = fetchFromGitHub {
     owner = "skanehira";
     repo = "version-lsp";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-L/j0CtowcyhohCp8ia1Jd6apq+0KSfE18+exH/ghLP4=";
+    rev = "04fecc58a92a3af9ecf412db96898b0ab3d352e4";
+    hash = "sha256-+Al/g+yEQlsE3KaMsouRGPn/si1k+YqwjRIr+2VqIzk=";
   };
 
-  cargoHash = "sha256-/6DKJpuBfvNfGAnfSaZjzZozpXK9m8kqVVcO9ubn91Y=";
+  cargoHash = "sha256-/HjTxKo5zM533FFhgTOCnnwvc0qBx0dLNL3l9WFsAjk=";
 
-  nativeBuildInputs = [
-    pkg-config
-    makeBinaryWrapper
-  ];
-
-  buildInputs = [
-    openssl
-  ];
-
-  postInstall = ''
-    wrapProgram $out/bin/version-lsp \
-      --set SSL_CERT_FILE ${cacert}/etc/ssl/certs/ca-bundle.crt
-  '';
+  buildInputs = [ openssl ];
+  nativeBuildInputs = [ pkg-config ];
 
   doCheck = false;
 
   meta = {
-    description = "Language Server Protocol (LSP) implementation that provides version checking diagnostics for package dependency files";
+    description = "A Language Server Protocol (LSP) implementation that provides version checking diagnostics for package dependency files";
     homepage = "https://github.com/skanehira/version-lsp";
     license = lib.licenses.mit;
     mainProgram = "version-lsp";
