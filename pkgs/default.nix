d818aa7dc8f9b0076ae0a3ed3b54df220ace9b09
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-05-20 15:27:03 +0900

    chore: remove packages registered in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,14 +2,10 @@ pkgs:
 pkgs.lib.makeScope pkgs.newScope (self: {
   bkm = self.callPackage ./bkm { };
   blake = self.callPackage ./blake { };
-  cell = self.callPackage ./cell { };
-  dockerfile-pin = self.callPackage ./dockerfile-pin { };
   ghqr = self.callPackage ./ghqr { };
   git-ombl = self.callPackage ./git-ombl { };
   home-manager-prune = self.callPackage ./home-manager-prune { };
-  opensrc = self.callPackage ./opensrc { };
   pim = self.callPackage ./pim { };
-  similarity = self.callPackage ./similarity { };
   textlint-filter-rule-comments = self.callPackage ./textlint-filter-rule-comments { };
   textlint-rule-preset-ai-writing = self.callPackage ./textlint-rule-preset-ai-writing { };
   textlint-rule-preset-japanese = self.callPackage ./textlint-rule-preset-japanese { };
