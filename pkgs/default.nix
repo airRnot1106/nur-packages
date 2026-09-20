f25946f3c3d4e1b75d735c8082d5266dfaef235a
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-09-20 13:39:30 +0900

    chore: remove unused packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,17 +1,12 @@
 pkgs:
 pkgs.lib.makeScope pkgs.newScope (self: {
   bkm = self.callPackage ./bkm { };
-  blake = self.callPackage ./blake { };
   css-modules-kit-ts-plugin = self.callPackage ./css-modules-kit-ts-plugin { };
   dockerfile-roast = self.callPackage ./dockerfile-roast { };
   ghasec = self.callPackage ./ghasec { };
-  ghqr = self.callPackage ./ghqr { };
-  git-ombl = self.callPackage ./git-ombl { };
   home-manager-prune = self.callPackage ./home-manager-prune { };
   jev-lint = self.callPackage ./jev-lint { };
   moine = self.callPackage ./moine { };
-  nothing-less = self.callPackage ./nothing-less { };
-  pim = self.callPackage ./pim { };
   pumlv = self.callPackage ./pumlv { };
   stylelint-language-server = self.callPackage ./stylelint-language-server { };
   suiko = self.callPackage ./suiko { };
@@ -19,9 +14,6 @@ pkgs.lib.makeScope pkgs.newScope (self: {
   textlint-rule-preset-ai-writing = self.callPackage ./textlint-rule-preset-ai-writing { };
   textlint-rule-preset-japanese = self.callPackage ./textlint-rule-preset-japanese { };
   uncomment = self.callPackage ./uncomment { };
-  version-lsp = self.callPackage ./version-lsp { };
   vikokoro = self.callPackage ./vikokoro { };
-  volt = self.callPackage ./volt { };
   yaskkserv2 = self.callPackage ./yaskkserv2 { };
-  zerocast = self.callPackage ./zerocast { };
 })
